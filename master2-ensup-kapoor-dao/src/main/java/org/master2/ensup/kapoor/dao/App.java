c8d5c02aab5dbea9f73ffff3924e411ea953f129
Benjamin KAPOOR <benjaminkapoor@yahoo.fr>
2019-10-31 11:49:41 +0100

    Commit projet master2 ensup kapoor

---
+++ master2-ensup-kapoor-dao/src/main/java/org/master2/ensup/kapoor/dao/App.java
@@ -0,0 +1,13 @@
+package org.master2.ensup.kapoor.dao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ master2-ensup-kapoor-domaine/src/main/java/org/master2/ensup/kapoor/domaine/App.java
@@ -0,0 +1,13 @@
+package org.master2.ensup.kapoor.domaine;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
